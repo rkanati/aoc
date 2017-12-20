0baec2fcad53d572e923b933df22b51f11264b70
Rachel K <raech.kanati@gmail.com>
2017-12-20 03:33:15 +0000

    day 4 p2

--- 4/phrases.hs
+++ 4/phrases.hs
@@ -1,8 +1,14 @@
 
-import Data.List (nub)
+import Data.List (nub, nubBy, sort)
 
 main :: IO ()
-main = putStrLn.show.length.filter valid =<< phrases where
-  phrases = (map words . lines) <$> readFile "input"
-  valid ws = length ws == length (nub ws)
+main = do
+  phrases <- (map words . lines) <$> readFile "input"
+  putStrLn.show.length.filter noDups $ phrases
+  putStrLn.show.length.filter noAnas $ phrases
+
+  where
+  noDups ws = length ws == length (nub ws)
+  noAnas ws = length ws == length (nubBy isAnagram ws)
+  isAnagram a b = sort a == sort b
 
