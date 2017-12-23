e22d2fc471369bae2b21b1b8258b74a15336d7c8
Rachel K <raech.kanati@gmail.com>
2017-12-23 15:48:06 +0000

    day 7 p2

--- 7/balance.hs
+++ 7/balance.hs
@@ -3,8 +3,10 @@ import Data.Char (isAlpha)
 import Data.List (mapAccumL, intercalate)
 import Data.Either (partitionEithers)
 import Data.Maybe (fromJust)
+import Data.Foldable (foldr')
+import Data.Monoid (Sum(..))
 
-data Node = Node String Int [Node]
+data Node a = Node String a [Node a]
 
 data RefNode = RefNode String Int [String]
 
@@ -18,37 +20,42 @@ indent lvs = go True lvs where
       (False, True ) -> "└"
       (False, False) -> "  "
 
-showNode :: [Bool] -> Node -> String
+showNode :: Show a => [Bool] -> Node a -> String
 showNode is (Node n w cs) = indent is ++ n ++ " " ++ show w
   ++ intercalate "\n" ("":showKids cs)
   where showKids []     = []
         showKids [c]    = [showNode (False:is) c]
         showKids (c:cs) = showNode (True:is) c : showKids cs
 
-instance Show Node where
+instance Show a => Show (Node a) where
   show = showNode []
 
-argmax f xs = fmap fst $ foldr' choose Nothing $ zip xs (map f xs) where
-  choose Nothing        (x', fx') = Just (x', fx')
-  choose (Just (x, fx)) (x', fx') =
-    Just $ if fx' > fx then (x', fx') else (x, fx)
+argMax f xs = fmap fst $ foldr' choose Nothing $ zip xs (map f xs) where
+  choose b          Nothing          = Just b
+  choose b@(_, fx') (Just a@(_, fx)) = Just $ if fx' > fx then b else a
 
-findImbalance :: Node -> Int
+findImbalance :: Node Int -> Int
 findImbalance = go 0 . weighTree where
-  go target (Node _ w []) = target - w
-  go target (Node _ w cs) =
-    case argMax (abs.go target') cs of
-      Just dw -> dw
-      Nothing -> 
-    target' = (target - w) `div` (length cs)
+  go target (Node _ (w, w') cs) = dw where
+    dw =
+      if all (== maxw) cws
+        then w + err
+        else if err > 0
+          then go maxw (fromJust $ argMax (negate.getWeight) cs)
+          else go minw (fromJust $ argMax         getWeight  cs)
+    err = target - w'
+    cws = map getWeight cs
+    minw = minimum cws
+    maxw = maximum cws
 
-  weighTree (Node n w cs) = Node n w' cs' where
+  weighTree (Node n w cs) = Node n (w, w') cs' where
     cs' = map weighTree cs
-    w' = w + foldMap getWeight cs'
+    w' = w + getSum (foldMap (Sum . getWeight) cs')
 
-  getWeight (Node _ w _) = w
+  getWeight      (Node _ (_, w) _) = w
+  getTowerWeight (Node _ (w, _) _) = w
 
-buildTree :: [Node] -> [RefNode] -> Node
+buildTree :: [Node Int] -> [RefNode] -> Node Int
 buildTree [n] [] = n
 buildTree ns (RefNode n w cs : rs) =
   case sequence mcs' of
@@ -63,7 +70,7 @@ buildTree ns (RefNode n w cs : rs) =
     where
     pred (Node s _ _) = s == c
 
-parseNode :: [String] -> Either Node RefNode
+parseNode :: [String] -> Either (Node Int) RefNode
 parseNode (n:w:_:cs) = Right $ RefNode n (read w) (map uncomma cs) where
   uncomma = filter isAlpha
 parseNode (n:w:[]) = Left $ Node n (read w) []
@@ -74,4 +81,5 @@ main = do
   let (leaves, refs) = partitionEithers table
   let root = buildTree leaves refs
   putStrLn $ show root
+  putStrLn $ show (findImbalance root)
 
